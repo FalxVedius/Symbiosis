31f660961348eb9d52f686f5a0cdb8656e327bc1
Alex Ortiz <AlexJOrtiz7430@gmail.com>
2020-07-15 13:01:45 -0600

    Added Basic Debug Menu UI on Player UI
    Added Debug Menu to Player UI and added restart level and load next level buttons and functionality

---
+++ Assets/Scripts/UI Scripts/DebugMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugMenu : MonoBehaviour
+{
+    #region Public Variables
+    public GameObject debugMenu;
+    #endregion
+
+    #region Private Variables
+    bool isActive;
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            if(isActive == false)
+            {
+                ShowDebug();
+            }
+            else
+            {
+                hideDebug();
+            }
+        }
+    }
+    #region Private Functions
+    void ShowDebug()
+    {
+        isActive = !isActive;
+        debugMenu.SetActive(true);
+    }
+
+    void hideDebug()
+    {
+        isActive = !isActive;
+        debugMenu.SetActive(false);
+    }
+    #endregion
+
+    #region Public Functions
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Loading Next Level");
+    }
+
+    public void RestartLevel()
+    {
+        string scene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(scene);
+        Debug.Log("Reloading " + scene);
+    }
+    #endregion
+}
