cf2363793090efa04b7f2326012d90dce5b29cdf
Alex Ortiz <AlexJOrtiz7430@gmail.com>
2020-07-18 08:15:48 -0600

    Created Basic Audio Manager
    Created Basic Audio manager and audio Slider script

---
+++ Assets/Scripts/UI Scripts/AudioSlider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSlider : MonoBehaviour
+{
+    AudioManager audioManager;
+    Slider slider;
+    float musicVol;
+    float sfxVol;
+
+    public void Awake()
+    {
+        slider = this.gameObject.GetComponent<Slider>();
+        if(slider == null)
+        {
+            Debug.LogError("Slider not assigned");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = AudioManager.instance;
+
+        if (audioManager == null)
+        {
+            Debug.Log("Audio Manager not assigned");
+        }
+        else
+        {
+            if (this.gameObject.name == "BackgroundSlider")
+            {
+                slider.value = audioManager.GetMusicVolume();
+
+            }
+            else if (this.gameObject.name == "SFXSlider")
+            {
+                slider.value = audioManager.GetSFXVolume();
+                Debug.Log("SFX Slider vold Assigned");
+            }
+            else
+            {
+                Debug.Log(gameObject.name + " doesn't have proper slider name on it");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
